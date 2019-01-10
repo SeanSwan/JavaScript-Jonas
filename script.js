b42c0474c8ccef78c48b5121391693af0ece4019
Sean Swan <ogpswan@YAHOO.COM>
2019-01-09 18:10:10 -0800

    variable coercion and mutaion alert and prompts

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/*************************************
+* VARIABLES AND DATA TYPES
+*/
+/*
 const firstName = 'john';
 console.log(firstName);
 
@@ -13,6 +17,35 @@ console.log(job);
 job = 'Personal Trainer';
 console.log(job);
 
+// Variable Naming Rules
 var _3years = 3;
 var johnMark = 'John and Mark';
+*/
 
+/*****************************************
+ * VARIABLE MUTATION AND TYPE COERCION
+ * */
+ 
+var firstName = 'John';
+var age = 28;
+
+//TYPE COERCION
+console.log(firstName + ' ' + age);
+
+//BEST PRATICE IS TO ADD ALL VARIABLES ON ONE LINE BY ADDING A COMMA
+var job, isMarried;
+job = 'Personal Trainer'
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//VARIBLE MUTATION = CHANGING THE VALUE OF AN VARIABLE
+age = 'twenty eight';
+job = 'driver';
+
+//ALERT ALTERNATIVE VERSION TO CONSOLE.LOG
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+  //USING PROMPT FOR STORING USER DATA RECIEVING AN ANSWER TO A QUESTION ETC..
+ var lastName = prompt('What is his last Name');
+ console.log(firstName + ' ' + lastName);
