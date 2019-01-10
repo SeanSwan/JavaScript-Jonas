aaffbf01b69419bbad4f29969e893cfb1ca16163
Sean Swan <ogpswan@YAHOO.COM>
2019-01-10 12:49:48 -0800

    coding challange 2

--- script.js
+++ script.js
@@ -245,7 +245,7 @@ console.log(drink);
 
 
 //SWITCH STATEMENT
-var job = 'teacher';
+/*var job = 'teacher';
 switch (job) {
   case 'teacher':
   case 'instructer':
@@ -276,4 +276,56 @@ switch (true) {
   break;
   default:
   console.log(firstName + ' is a man.');
-}
+}*/
+
+/************************************************
+ * TRUTY AND FALSY VALUES AND EQUALITY OPERATORS
+ */
+
+ //falsy values: undefined, null, 0, '', NaN
+ //truthy values: NOT falsy values
+
+ var height;
+
+ height = 23;
+ if (height || height === 0) {
+   console.log('Variable is defined');
+ } else {
+   console.log('Variable has NOT been defined');
+ }
+
+ // Equality Operators
+ if (height == '23') {
+   console.log('The == operator dose type coercion!');
+ }
+
+ /*******************************************
+  * CODING CHALLENGE 2
+  */
+
+  /* 
+  John and Mike both play basketball on different 
+  teams. In the latest 3 games, John's team scored 89, 
+  120 and 103 points, while Mike's team scored 116, 94
+  and 123 points.
+
+  1. Calculate the average score for each team
+  2. Decide which teams wins in average (highest 
+    average score), and print the winner on the console.
+    Also include the average score in the output.
+    3. Then change the scores to show different winners.
+    Don't forget to take into account there might be a
+    draw (the same average score)
+
+    4. EXTRA: Mary also plays basketball, and her team
+    scored 97, 134 and 105 points. Like before,log the
+    average winner to the console. HINT: you will need
+    the && operator to take the dicision. If you can't
+    solve this one, just watch the solution, it's no
+    problem :)
+    5. Like before, change the scores to generate
+    different winners, keeping in mind there might be
+    draws.
+
+    
+    */
